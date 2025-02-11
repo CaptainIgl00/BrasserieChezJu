3bbb63dfc936583e3e5e7d249d9a06334f3b65dc
Mathéo Guilloux <matheo.guilloux@gmail.com>
2025-02-11 18:36:41 +0100

    feat: preconnect fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -54,6 +54,8 @@ export default defineNuxtConfig({
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
       link: [
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
         { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
         { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
